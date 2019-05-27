d8484ff146c95e25c136ff08d430ea09214928ec
JEAN GABRIEL DA SILVA MEDEIROS <ALU201618010@uniritter.local>
2019-05-27 11:20:33 -0300

    Receita de Acarajé

---
+++ src/main/Acaraje.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Acaraje implements Receita {
+	
+	@Override
+	public String receita() {
+		return "Coloque os feijões inteiros num processador de alimentos e bata por alguns segundos para quebrar os grãos.\nCuidado para não esmigalhar.\nColoque os feijões num recipiente e cubra com água.\nDeixe de molho por no mínimo 12 horas.\nAgite os feijões com uma colher e com uma peneirinha vá retirando todas as cascas que se desprendem dos feijões.\nDepois de retirar bastante as cascas, lave os feijões sob a água corrente e continue retirando ?o olhinho? e o restante das cascas.\nRetire o máximo possível.\nReserve.\nDescasque as cebolas.\nCorte em pedaços.\nReserve.\nNum processador, coloque os feijões e as cebolas.\nBata por 3 minutos, ou até obter uma pasta lisa e uniforme.\nRetire toda a pasta do processador e coloque dentro de uma panela grande e funda.\nCom uma colher de pau, bata a massa do acarajé até que a massa triplique de volume.\nEsse processo é bastante demorado mas é importantíssimo para a fermentação.\nColoque o óleo, o azeite de dendê, e a cebola com casca numa panela média e leve ao fogo alto para esquentar.\nCom duas colheres de sopa, modele os acarajés: encha uma das colheres com a massa, passe de uma colher para a outra até que a massa fique com formato de bolinho.\nColoque os bolinhos no óleo bem quente e frite por 3 minutos de um lado.\nCom uma escumadeira, vire os bolinhos e deixe fritar até que fiquem dourados.\nRegule a temperatura do óleo.\nSe for necessário, abaixe a chama do fogo para que o óleo não queime.\nRetire os bolinhos com uma escumadeira e coloque sobre papel absorvente.\nCorte os bolinhos ao meio e recheie com vatapá.\nColoque o camarão seco dentro do acarajé e feche.\nSirva a seguir.\n";
+	}
+
+	@Override
+	public String prato() {
+		return "Acarajé, oxente, que treco bão!";
+	}
+}
